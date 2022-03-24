984e6c2e846214db2ef30d3654200480da2f3bff
jennahabel <86261801+jennahabel@users.noreply.github.com>
2022-03-23 23:03:08 -0600

    Final Commit
    Final Commit

--- project2/Startup.cs
+++ project2/Startup.cs
@@ -56,7 +56,7 @@ namespace project2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{appointmentId?}");
             });
         }
     }
